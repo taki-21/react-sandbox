32165f615c74603f75a3f8558dd3bb7ef2d389cf
taki-21 <taki.it.0208.21@gmail.com>
2021-12-31 09:58:17 +0900

    add: ts-react-basic

---
+++ ts-react-basic/src/Todo.tsx
@@ -0,0 +1,11 @@
+import { TodoType } from "./types/todo";
+import { FC } from "react";
+
+export const Todo: FC<Omit<TodoType, "id">> = (
+  props
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー: ${userId} )`}</p>;
+};
