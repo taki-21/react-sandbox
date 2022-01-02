add03caaf74b7c0268beb39cce73b59b9abe9c28
taki-21 <taki.it.0208.21@gmail.com>
2022-01-03 03:00:46 +0900

    add: ユーザー一覧画面の追加

--- ts-react-user-management-app/src/components/pages/UserManagement.tsx
+++ ts-react-user-management-app/src/components/pages/UserManagement.tsx
@@ -1,5 +1,34 @@
-import { FC, memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { UserCard } from "../organism/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
---
+++ ts-react-user-management-app/src/hooks/useAllUsers.ts
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useCallback } from "react";
+import { User } from "../types/api/user";
+import axios from "axios";
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+  const { showMessage } = useMessage();
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState<Array<User>>([]);
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch(() => {
+        showMessage({ title: "ユーザー取得に失敗しました", status: "error" });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return { getUsers, loading, users };
+};
